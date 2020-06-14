61384d2601d61f432caacc5177aaee8d80bdbc21
Rahul Singh <rsembedded@gmail.com>
2020-06-14 17:22:23 +0530

    7-working_with_class_and_instances_data.py added

---
+++ Python Beyond the Basics - Object-Oriented Programming/3. Classes/9-working_with_class_and_instance_data.py
@@ -0,0 +1,47 @@
+"""
+how to decide which data put in class this is decide when we put and decide the 
+   Or
+How to work object and class data and access
+"""
+val = 10  # global variable can not used in the class so we need to define in the class
+
+
+class MyClass(object):
+    count = 0
+    
+    def __init__(self, value):
+        self.val = value
+        MyClass.count += 1
+
+    def set_val(self, new_value):
+        try:
+            value = int(new_value)
+        except ValueError:
+            return
+        self.val = new_value
+
+    def get_val(self):
+        return self.val
+
+    def get_count(self):
+        return MyClass.count
+
+
+a = MyClass(10)
+b = MyClass(50)
+c = MyClass(100)
+
+for i in (a, b, c):
+    print("val of object", i.get_val())
+    print("Count",i.get_count())
+
+
+
+
+
+
+
+
+
+
+
